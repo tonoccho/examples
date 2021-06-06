6cbeaf613f004bec64cc82ac582d922c57328e87
seiji tonouchi <s.tonouchi@gmail.com>
2021-06-07 11:50:37 +1200

    mvnw

--- src/main/java/org/tonouchi/App.java
+++ src/main/java/org/tonouchi/App.java
@@ -4,9 +4,17 @@ package org.tonouchi;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
+public class App {
+    /**
+     * Do nothing
+     */
+    protected App(){}
+
+    /**
+     * Main method
+     * @param args command line arguments
+     */
+    public static void main(final String[] args)
     {
         System.out.println( "Hello World!" );
     }
