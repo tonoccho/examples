aafd73f82e7315c1aae1a3bffacf787a5f9cd54c
seiji tonouchi <s.tonouchi@gmail.com>
2021-06-09 06:42:19 +1200

    hoge

--- src/test/java/org/tonouchi/AppTest.java
+++ src/test/java/org/tonouchi/AppTest.java
@@ -1,11 +1,10 @@
 package org.tonouchi;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
@@ -27,5 +26,21 @@ public class AppTest
     @Test
     public void testAdd() {
         assertEquals(2, app.add(1, 1));
+    }
+
+    @Test
+    public void testAdd2() {
+        assertEquals(3, app.add2(2, 1));
+    }
+
+    @Test
+    public void testAdd3() {
+        assertEquals(8, app.add3(3, 5));
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testAdd3_numm1MustBeSmallerThanNum2() {
+        app.add3(3, 2);
+        fail();
     }
 }
